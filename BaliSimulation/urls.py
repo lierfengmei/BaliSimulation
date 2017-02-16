b72058370491be0eaa7913bb93f489757b3402d9
lierfengmei <lifengmei89@sina.com>
2017-02-16 13:20:29 +0800

    first time can python manager.py runserver

--- BaliSimulation/urls.py
+++ BaliSimulation/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from bali import views
+
 
 urlpatterns = [
+    url(r'^$',views.index,name="index"),
     url(r'^admin/', admin.site.urls),
 ]
--- bali/views.py
+++ bali/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index1(request):
+    return HttpResponse("Here is the index page!")
+
+def index(request):
+    return render(request,'base_rango.html',{})
