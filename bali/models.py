6391a6e8709e7452258109faae130d02b839f9a2
lierfengmei <lifengmei89@sina.com>
2017-04-20 17:12:06 +0800

    know how to transfer data

--- bali/models.py
+++ bali/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class AddNumber(models.Model):
+    addNumber1 = models.IntegerField()
+    addNumber2 = models.IntegerField()
+
+    def __str__(self):
+        return str(self.addNumber1)
--- bali/views.py
+++ bali/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import AddForm
+
+
 
 def index(request):
-    return render(request,'bali/index.html',{})
+    if request.method == 'POST':#提交表单时
+        form = AddForm(request.POST) #form包含提交的数据
+        if form.is_valid():
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            data = str(int(a)+int(b))
+            return render(request,'bali/readBalise.html',{'data':data,'form':form})
+            #return HttpResponse(str(int(a)+int(b)))
+    else:#当正常访问时
+        form = AddForm()
+    return render(request,'bali/readBalise.html',{'form':form})
 
 
 def faq(request):
